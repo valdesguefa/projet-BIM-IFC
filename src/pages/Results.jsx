ecd92df5504e181021954d1408263c882bea4f86
valdesguefa <valdesguefa@gmail.com>
2022-08-09 17:50:01 +0100

    ajout des resultats, des notifications et debut d'implementation de ccvd

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,6 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom";
 
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+//import { fadeIn } from 'react-animations'
+
+import Radium, {StyleRoot} from 'radium';
+
+// const styles = {
+//   bounce: {
+//     animation: 'x 1s',
+//     animationName: Radium.keyframes(fadeIn, 'bounce')
+//   }
+// }
+
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
 export default function Results(props) {
 
 
@@ -25,27 +52,27 @@ export default function Results(props) {
   }
 
   useEffect(() => {
-    console.log('props.constructDate',props.constructDate)
+    console.log('props.constructDate', props.constructDate)
 
     var a = 0
     var d = props.actualisationReel / 100
     a = Actu(props.utilisation, props.actualisation, d, props.persistance)
-     console.log('setCpersistance', a)
+    console.log('setCpersistance', a)
     setCpersistance(a)
 
 
     var b = 0
-     console.log('props.dementellement',props.dementellement)
-     console.log('props.persistance',props.persistance)
-     console.log('props.actuali0ationReel',props.actualisationReel)
+    console.log('props.dementellement', props.dementellement)
+    console.log('props.persistance', props.persistance)
+    console.log('props.actuali0ationReel', props.actualisationReel)
     b = Actu(props.dementellement, props.persistance, d, props.persistance)
     b = b / (props.persistance)
-     console.log('setCdementellement', b)
+    console.log('setCdementellement', b)
     setCdementellement(b)
 
     var c = 0
     c = a + b + props.investissement
-     console.log('setccvs', c)
+    console.log('setccvs', c)
     setccvs(c)
 
 
@@ -63,7 +90,7 @@ export default function Results(props) {
         </div>
         <span>              Nom	:	{props.nom}</span>
         <span>          Nature	:	{props.nature}</span>
-         <span>Date de livraison	:		{props.constructDate}</span> 
+        <span>Date de livraison	:		{props.constructDate}</span>
         <span>          Surface	:	{props.constructionArea}</span>
         <span>  Mode d'analyse	:	{props.checkedSimple ? 'Global' : 'Detaille'}	</span>
 
@@ -95,7 +122,29 @@ export default function Results(props) {
       <div>
 
       </div>
-
+      <Box sx={{ minWidth: 175 }}>
+      <Card sx={{ minWidth: 10 }}>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Word of the Day
+          </Typography>
+          <Typography variant="h5" component="div">
+            be{bull}nev{bull}o{bull}lent
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            adjective
+          </Typography>
+          <Typography variant="body2">
+            well meaning and kindly.
+            <br />
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+      </Box>
     </div>
   )
 }
